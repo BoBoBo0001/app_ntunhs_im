649194d0486fb84e4d162f06f84ed1e0a4feb2d7
bo <128464844+BoBoBo0001@users.noreply.github.com>
2024-03-07 16:50:16 +0800

    hw2
    222

--- guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.guess_number
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = MainActivity::class.java.simpleName
+    private lateinit var  handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handler = Handler(Looper.getMainLooper())
 
         val textView = findViewById<TextView>(R.id.textView)
         val editText = findViewById<EditText>(R.id.editText)
@@ -22,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         button1.setOnClickListener{
             num = editText.text.toString().toInt() - secret
-            var ans_str : String = "你猜對了"
+            var ans_str : String = "BINGO"
 
             if(num > 0){
                 max = editText.text.toString().toInt()
@@ -31,6 +38,15 @@ class MainActivity : AppCompatActivity() {
             else if(num < 0){
                 min = editText.text.toString().toInt()
                 ans_str = min.toString() +"~"+ max.toString()
+            }
+            else{
+                secret = Random().nextInt(100)+1
+                max = 100
+                min = 1
+                handler.postDelayed({
+                    Toast.makeText(this,"經過6秒，已重置!",Toast.LENGTH_SHORT).show()
+                },6000)
+                textView.text = "結果會顯示在這裡"
             }
             textView.text = ans_str
         }
@@ -42,6 +58,10 @@ class MainActivity : AppCompatActivity() {
             textView.text = "結果會顯示在這裡"
             Toast.makeText(this,"數字已經重置",Toast.LENGTH_SHORT)
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
